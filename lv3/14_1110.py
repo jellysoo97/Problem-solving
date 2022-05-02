680628f3330e4f539a3f2149cf8286ab71f3cdef
jellysoo97 <jellysoo97@gmail.com>
2022-05-02 21:28:14 +0900

    0502 수정

--- lv3/14_1110.py
+++ lv3/14_1110.py
@@ -1,9 +1,9 @@
 # 더하기 사이클
 # 질문1 : 주어진 수가 10보다 작은 경우를 안따져도 되는가? 밑의 코드는 어디가 잘못된건지?
 
-
 first = int(input())
 num = first
+new = 0
 cnt = 0
 
 while True:
@@ -11,11 +11,11 @@ while True:
     num *= 10
     new = (num%10) * 10 + (num//10 + num%10) % 10
   else:
-    new = (new%10) * 10 + (new//10 + new%10) % 10
+    new = (num%10) * 10 + (num//10 + num%10) % 10
   cnt += 1
+  num = new
   if num == first:
     break
-  num = new
 
 print(cnt)
 
