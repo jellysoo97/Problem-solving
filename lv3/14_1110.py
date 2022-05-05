2b7bae7465147e51ebcad837b074fe54c08151c7
jellysoo97 <jellysoo97@gmail.com>
2022-05-05 17:33:28 +0900

    lv3

--- lv3/14_1110.py
+++ lv3/14_1110.py
@@ -1,5 +1,7 @@
 # 더하기 사이클
 # 질문1 : 주어진 수가 10보다 작은 경우를 안따져도 되는가? 밑의 코드는 어디가 잘못된건지?
+# 피드백 : 되도록 종료조건을 while문의 맨 마지막에 -> but 항상 그런건 아님
+# 나머지로 구현하면 어차피 조건이 당연하게 해결됨 so, 안따져도 ok
 
 first = int(input())
 num = first
@@ -14,7 +16,7 @@ while True:
     new = (num%10) * 10 + (num//10 + num%10) % 10
   cnt += 1
   num = new
-  if num == first:
+  if num == first: 
     break
 
 print(cnt)
